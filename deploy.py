c92d9d2dde7944556b896e4cd4600f3e127f0ab7
Aakash B <baakash@corp.issimail.com>
2026-05-14 09:41:21 -0500

    test2

--- deploy.py
+++ deploy.py
@@ -1,11 +1,15 @@
 import sys
+from azure.identity import DefaultAzureCredential
 from fabric_cicd import FabricWorkspace, publish_all_items
 
 workspace_name = sys.argv[sys.argv.index('--workspace_name') + 1]
 
+credential = DefaultAzureCredential()
+
 workspace = FabricWorkspace(
     workspace_name=workspace_name,
     repository_directory=workspace_name,
-    item_type_in_scope=["Report", "SemanticModel"]
+    item_type_in_scope=["Report", "SemanticModel"],
+    token_credential=credential
 )
 publish_all_items(workspace)
